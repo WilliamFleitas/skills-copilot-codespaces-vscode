cf2b25156fe9fe780d4a95881819f69b05c8834d
William Fleitas <69325009+WilliamFleitas@users.noreply.github.com>
2023-11-01 05:18:20 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var skill = document.getElementById("skill").value;
+    var skillList = document.getElementById("skillList");
+    var skillItem = document.createElement("li");
+    skillItem.appendChild(document.createTextNode(skill));
+    skillList.appendChild(skillItem);
+}
