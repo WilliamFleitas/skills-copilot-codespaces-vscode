1f11b0834fec2651536d378a60e97a932dc83696
William Fleitas <69325009+WilliamFleitas@users.noreply.github.com>
2023-11-01 05:45:01 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,31 @@
 // Create a web server
-// Use express framework to create a web server
+// 1. Create a web server that listens to port 3000
+// 2. Handle requests to the root path (/) with a simple "Hello World" message
+// 3. Handle requests to the /comments path by sending the comments array
+// 4. Handle requests to any other path with a 404 status code and a "Not found" message
+
 const express = require('express');
 const app = express();
-const port = 3000;
-const path = require('path');
 
-// Use ejs as the view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+const comments = [
+    { username: 'Tammy', comment: 'lol that is so funny!' },
+    { username: 'FishBoi', comment: 'Pls delete this post.' },
+    { username: '4EvaInnocent', comment: 'reported for spam.' },
+    { username: 'SallyWest', comment: 'This is fake news.' },
+];
 
-// Use express.static() to serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
-// Use the 'comments' route
-app.use('/comments', require('./routes/comments'));
+app.get('/comments', (req, res) => {
+    res.send(comments);
+});
 
-// Start the server
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.get('*', (req, res) => {
+    res.status(404).send('Not found');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
